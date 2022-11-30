a368b20b1d9846a96b27ae5689effb71e0a4a6fa
Wade Croft <wadecroft@gmail.com>
2022-11-30 14:10:31 -0800

    add vanguard files

--- page.php
+++ page.php
@@ -1,2 +1,14 @@
 <?php
-// Silencio
+	global $post;
+	$front_end_url = get_frontend_url();
+	$slug = $post->post_name;
+	$type = $post->post_type;
+	$url = "{$front_end_url}/api/revalidate/{$slug}?type={$type}&secret=Z87ZfKnwgE9Jf3q6zaFjalw2";
+	$response = wp_remote_get($url);
+	echo $url;
+function pretty_print($arr)
+{
+  echo '<pre>';
+  print_r($arr);
+  echo '</pre>';
+}
